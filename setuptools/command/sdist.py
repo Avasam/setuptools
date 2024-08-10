7de81b92650647bd0d7f13cc3e25f6a2c4d91943
Avasam <samuel.06@hotmail.com>
2024-08-09 23:25:13 -0400

    Update setuptools/command/sdist.py

--- setuptools/command/sdist.py
+++ setuptools/command/sdist.py
@@ -47,7 +47,7 @@ class sdist(orig.sdist):
     ]
 
     distribution: Distribution  # override distutils.dist.Distribution with setuptools.dist.Distribution
-    negative_opt: ClassVar[dict[str, str]] = {}  # type: ignore[misc] # Fixed upstream in typeshed to be a ClassVar
+    negative_opt: ClassVar[dict[str, str]] = {}  # type: ignore[misc] # Fixed upstream in typeshed to be a ClassVar. Should be included in mypy 1.12
 
     README_EXTENSIONS = ['', '.rst', '.txt', '.md']
     READMES = tuple('README{0}'.format(ext) for ext in README_EXTENSIONS)
