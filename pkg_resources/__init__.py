5fe67c5dc4555d69a7898d010af6f92853e7dd57
Avasam <samuel.06@hotmail.com>
2024-08-30 11:17:59 -0400

    space in type-ignore

--- pkg_resources/__init__.py
+++ pkg_resources/__init__.py
@@ -2649,7 +2649,7 @@ def _cygwin_patch(filename: StrOrBytesPath):  # pragma: nocover
     that this seems to be by design...
     """
     if sys.platform == 'cygwin':
-        return os.path.abspath(filename)  # type:ignore[type-var]
+        return os.path.abspath(filename)  # type: ignore[type-var]
     return filename
 
 
@@ -3356,7 +3356,7 @@ class Distribution:
             kw.setdefault(attr, getattr(self, attr, None))
         kw.setdefault('metadata', self._provider)
         # Unsafely unpacking. But keeping **kw for backwards and subclassing compatibility
-        return self.__class__(**kw)  # type:ignore[arg-type]
+        return self.__class__(**kw)  # type: ignore[arg-type]
 
     @property
     def extras(self):
